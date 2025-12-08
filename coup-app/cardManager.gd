94524a3575616c317070afb3333d63217808fbfc
Seleek <148657074+Seleek@users.noreply.github.com>
2025-12-07 17:46:08 -0700

    Las cartas se arrastran

--- coup-app/cardManager.gd
+++ coup-app/cardManager.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 var card_being_dragged
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_pos = get_global_mouse_position()
-		card_being_dragged = mouse_pos
+		card_being_dragged.position = mouse_pos
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card			
+				card_being_dragged = card
 		else:
 			card_being_dragged = null
 
@@ -23,7 +24,7 @@ func raycast_check_for_card():
 	parameters.collide_with_areas = true
 	parameters.collision_mask = 1
 	var result = space_state.intersect_point(parameters)
-	if result.size() > 0:
+	if result.size()>0:
 		return result[0].collider.get_parent()
 	return null
 
