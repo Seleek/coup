8fb4d15f61f20d50d8645cf8d5a38717ff1beb26
Seleek <148657074+Seleek@users.noreply.github.com>
2025-12-07 20:47:53 -0700

    3 jugadores prueba
    Se coloca todos los botones que se van a necesitar en la interfaz y se programa el nombre de jugador, el inicio y las monedas del juego para 3 jugadores

--- coup-app/GameData.gd
+++ coup-app/GameData.gd
@@ -1,5 +1,40 @@
 extends Node
 
+const MAX_INFLUENCE = 2
+const START_COINS = 2
+const INFLUENCE_TYPES = ["Duque", "Asesino", "Capitan", "Embajador", "Condesa"]
+
+var players = []
+var deck = []
+var treasury = 50
+var currently_player_index = 0
+
+#iniciar juego
+func init_game (player_names : Array):
+	for type in INFLUENCE_TYPES:
+		for i in range(3):
+			deck.append(type)
+	deck.shuffle()
+	
+	#iniciar jugadores
+	for name in player_names:
+		var new_player = {
+			"name": name,
+			"coins": START_COINS,
+			"influence": [draw_card(), draw_card()],
+			"revealed_influence":[],
+			"is_out": false
+		}
+		players.append(new_player)
+	currently_player_index=0
+
+func draw_card():
+	if deck.size()>0:
+		return deck.pop_back()
+	return null
+
+func get_current_player():
+	return players[currently_player_index]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
