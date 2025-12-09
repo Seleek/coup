809375805c8e05d17e2e44ccba2b6208f6539741
Seleek <148657074+Seleek@users.noreply.github.com>
2025-12-09 13:28:37 -0700

    Asigna datos a jugadores
    Se asigna los datos iniciales a cada jugador al empezar la partida

--- coup-app/game.gd
+++ coup-app/game.gd
@@ -1,11 +1,32 @@
-extends Node2D
+extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	GameData.init_game(["Jugador A", "Jugador B", "Jugador C"])
+	update_ui()
+	
+	func update_ui():
+		var current_player = GameData.get_current_player()
+		
+		var player_ui_nodes = $listaJugadores.get_children()
+		for i in range (GameData.players.size()):
+			var player_data = GameData.players[i]
+			var player_ui = player_ui_nodes[i]
+			
+			player_ui.get_node(nombrejugador).text = "%s (%s)" % ["**" + player_data.name + "**" if i == GameData.current_player_index else player_data.name, "Turno Actual" if i == GameData.current_player_index else ""]
+			player_ui.get_node(monedas).text = "Monedas: %d" % player_data.coins
+			
+			var card_contariner = player_ui.get_node("cartasInfluencia")
+			for j in range (MAX_INFLUENCE):
+				var card_node = card_container.get_child(j)
+				
+				if j < player.influence.size():
+					if i == GameData.current_player_index:
+						card_node.get_node("Label").text = player_data.indluence[j]
+					else:
+						card_node.get_node("Label").text = "Influencia"
+				else
+					card_node.get_node("Label").text = "PERDIDA"
+			
 func _process(delta: float) -> void:
 	pass
